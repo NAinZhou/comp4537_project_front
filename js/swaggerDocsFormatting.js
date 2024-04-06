730df7525229fe3e32a10546e0b50eaed8293323
John2T <johntu537@gmail.com>
2024-04-06 14:58:27 -0700

    Added url

--- js/swaggerDocsFormatting.js
+++ js/swaggerDocsFormatting.js
@@ -1,5 +1,5 @@
         function displaySwaggerUI() {
-            fetch('/API/v1/docs') // Make a GET request to the Flask endpoint
+            fetch('https://4537a01326006groupproject.online/API/v1/docs') // Make a GET request to the Flask endpoint
             .then((response) => {
                 if (!response.ok) {
                     throw new Error(`HTTP error! status: ${response.status}`);
